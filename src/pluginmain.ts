92a8c2b0a871181823e32112e8ceba8c72e56fef
Vladan Mitevski <vladan@mvdevelop.com>
2019-05-04 11:40:51 +0200

    Style accordion

--- src/pluginmain.ts
+++ src/pluginmain.ts
@@ -2,9 +2,9 @@
 // Demonstrates:
 // - typescript
 // - using trc npm modules and browserify
-// - uses promises. 
-// - basic scaffolding for error reporting. 
-// This calls TRC APIs and binds to specific HTML elements from the page.  
+// - uses promises.
+// - basic scaffolding for error reporting.
+// This calls TRC APIs and binds to specific HTML elements from the page.
 
 import * as XC from 'trc-httpshim/xclient'
 import * as common from 'trc-httpshim/common'
@@ -19,7 +19,7 @@ import * as trchtml from 'trc-web/html'
 
 // Installed via:
 //   npm install --save-dev @types/jquery
-// requires tsconfig: "allowSyntheticDefaultImports" : true 
+// requires tsconfig: "allowSyntheticDefaultImports" : true
 declare var $: JQueryStatic;
 
 // Provide easy error handle for reporting errors from promises.  Usage:
@@ -35,11 +35,11 @@ export class MyPlugin {
     private _pluginClient: plugin.PluginClient;
     private _opts: plugin.IPluginOptions;
 
-    // Scan for all <a> with "plugin" class and make into link. 
+    // Scan for all <a> with "plugin" class and make into link.
     // <a class="plugin">{PluginId}</a>
     private applyAllPlugins(): void {
         $("a[plugin]").each((idx, element) => {
-            // Text is the 
+            // Text is the
             var e = $(element);
             var text: string = element.innerText;
 
@@ -47,23 +47,23 @@ export class MyPlugin {
 
             if (!text || text.length == 0) {
                 element.innerText = "Use " + pluginId;
-            }           
+            }
 
             var url = this.getGotoLinkForPlugin(pluginId);
             e
-                .addClass("btn").addClass("btn-primary")
+                .addClass("btn").addClass("btn-blue").addClass("btn-small")
                 .attr("href", this.getGotoLinkForPlugin(pluginId))
                 .attr("target", "_blank");
         });
 
-        // TODO - remove this case. 
+        // TODO - remove this case.
         $("a.plugin").each((idx, e) => {
-            // Text is the 
+            // Text is the
             var pluginId: string = e.innerText;
             e.innerText = "Use " + pluginId;
             var url = this.getGotoLinkForPlugin(pluginId);
             $(e)
-                .addClass("btn").addClass("btn-primary")
+                .addClass("btn").addClass("btn-blue").addClass("btn-small")
                 .removeClass("plugin")
                 .attr("href", this.getGotoLinkForPlugin(pluginId))
                 .attr("target", "_blank");
@@ -101,14 +101,14 @@ export class MyPlugin {
         });
     }
 
-    // Expose constructor directly for tests. They can pass in mock versions. 
+    // Expose constructor directly for tests. They can pass in mock versions.
     public constructor(p: plugin.PluginClient) {
         this._sheet = new trcSheet.SheetClient(p.HttpClient, p.SheetId);
     }
 
 
-    // Make initial network calls to setup the plugin. 
-    // Need this as a separate call from the ctor since ctors aren't async. 
+    // Make initial network calls to setup the plugin.
+    // Need this as a separate call from the ctor since ctors aren't async.
     private InitAsync(): Promise<void> {
         return this._sheet.getInfoAsync().then(info => {
 
@@ -120,7 +120,7 @@ export class MyPlugin {
 
             return this._sheet.getChildrenAsync().then(childInfo => {
 
-                $("div[hasColumn]").each((idx, element) => {                   
+                $("div[hasColumn]").each((idx, element) => {
                     var e = $(element);
                     var cn = e.attr("hasColumn");
                     if (!dict.hasOwnProperty(cn.toLowerCase())) {
@@ -129,7 +129,7 @@ export class MyPlugin {
                 });
 
 
-                // Set styles 
+                // Set styles
                 document.title = "Voter-Science: " + info.Name;
 
 
@@ -141,7 +141,7 @@ export class MyPlugin {
                     $(".notTopLevel").show();
                 }
 
-                if (!!childInfo.entries && childInfo.entries.length > 0) 
+                if (!!childInfo.entries && childInfo.entries.length > 0)
                 {
                     $(".hasChildren").show();
                 } else {
@@ -162,5 +162,5 @@ export class MyPlugin {
 
         $("#LastRefreshed").text(new Date().toLocaleString());
     }
-  
+
 }
