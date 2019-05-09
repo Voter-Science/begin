341b667b7863fe101264c08df583c23ae309bbc5
Vladan Mitevski <vladan@mvdevelop.com>
2019-05-09 19:02:59 +0200

    add plus/minus icons for accordion UX improvement

--- public/main.js
+++ public/main.js
@@ -2,4 +2,12 @@ $(window).on("load", function() {
   if($('.block').length) {
     $('.equal').matchHeight();
   }
+
+  $('.collapse').on('show.bs.collapse', function () {
+    $(this).siblings('.panel-heading').addClass('active');
+  });
+
+  $('.collapse').on('hide.bs.collapse', function () {
+    $(this).siblings('.panel-heading').removeClass('active');
+  });
 });
